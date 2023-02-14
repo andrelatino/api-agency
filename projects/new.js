03af7808b6a80f2db1eab3884a31b539a4930019
roiwebco <91880878+NW360D@users.noreply.github.com>
2023-02-14 16:16:58 +0100

    v48
    Add dynamic data to projects forms

--- projects/new.js
+++ projects/new.js
@@ -69,3 +69,66 @@ function checkEmptyFields() {
 
   } 
 }
+const selectCustomers = document.querySelector('#customers_id');
+const selectAgencies = document.querySelector('#agencies_id');
+const selectFreelancers = document.querySelector('#freelancers_id');
+const selectManagers = document.querySelector('#managers_id');
+const selectServices = document.querySelector('#services_id');
+const selectTechnologies = document.querySelector('#technologies_id');
+const selectWorkflows = document.querySelector('#workflows_id');
+
+fetch('https://x8ki-letl-twmt.n7.xano.io/api:Nwul72Uo:v1/projects')
+  .then(response => response.json())
+  .then(data => {
+    
+      data.agencies.forEach(agency => {
+        const option = document.createElement('option');
+        option.value = agency.id;
+        option.textContent = agency.agencyName;
+        selectAgencies.appendChild(option);
+      });
+
+      data.freelancers.forEach(freelancer => {
+        const option = document.createElement('option');
+        option.value = freelancer.id;
+        option.textContent = freelancer.freelancerLastname;
+        selectFreelancers.appendChild(option);
+      });
+
+      data.customers.forEach(customer => {
+        const option = document.createElement('option');
+        option.value = customer.id;
+        option.textContent = customer.customerLastname;
+        selectCustomers.appendChild(option);
+      });
+
+      data.managers.forEach(manager => {
+        const option = document.createElement('option');
+        option.value = manager.id;
+        option.textContent = manager.managerLastname;
+        selectManagers.appendChild(option);
+      });
+
+      data.services.forEach(service => {
+        const option = document.createElement('option');
+        option.value = service.id;
+        option.textContent = service.serviceName;
+        selectServices.appendChild(option);
+      });
+
+      data.technologies.forEach(technology => {
+        const option = document.createElement('option');
+        option.value = technology.id;
+        option.textContent = technology.technologyName;
+        selectTechnologies.appendChild(option);
+      });
+
+      data.workflows.forEach(workflow => {
+        const option = document.createElement('option');
+        option.value = workflow.id;
+        option.textContent = workflow.workflowName;
+        selectWorkflows.appendChild(option);
+      });
+
+  })
+  .catch(error => console.error('Error fetching data:', error));
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_VERSION = "v47";
+var CACHE_VERSION = "v48";
 var CACHE = "neo-manager-" + CACHE_VERSION;
 
 // Call install event

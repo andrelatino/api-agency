114481739b97a7c678b368a00206370dc187479b
roiwebco <91880878+NW360D@users.noreply.github.com>
2023-02-28 23:30:14 +0100

    v54
    Fix project form

--- projects/new.js
+++ projects/new.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     setTimeout(function() {
-        hideProgressBar();
-        showForm();
+        showProgressBar();
+        loadFormContent();
     }, 1000);
   };
   function hideProgressBar() {
@@ -41,6 +41,7 @@ window.onload = function() {
         });
 
         if (response.status === 200) {
+            showForm();
             document.getElementById('formMessage').textContent = 'SUCCEED!';
             // closeAndReloadNew();
         } else {
@@ -49,7 +50,7 @@ window.onload = function() {
     } catch (error) {
         document.getElementById('formMessage').textContent = 'ERROR';
     }
-}
+  }
 
 function checkRequiredFields() {
   const form = document.getElementById('projectForm');
@@ -66,98 +67,102 @@ function checkRequiredFields() {
   if (allFieldsAreFilled) {
     addNewContent();
   } else {
-    alert('Please fill in all required fields.');
+    // alert('Please fill in all required fields.');
   }
 }
 
-const selectCustomers = document.querySelector('#customers_id');
-const selectManagers = document.querySelector('#managers_id');
-const selectServices = document.querySelector('#services_id');
-const selectTechnologies = document.querySelector('#technologies_id');
-const selectWorkflows = document.querySelector('#workflows_id');
-const selectTeams = document.querySelector('#teams_id');
-const selectAgencies = document.querySelector('#agencies_id');
-const selectFreelancers = document.querySelector('#freelancers_id');
-const selectAffiliates = document.querySelector('#affiliates_id');
+function loadFormContent(){
+  const selectCustomers = document.querySelector('#customers_id');
+  const selectManagers = document.querySelector('#managers_id');
+  const selectServices = document.querySelector('#services_id');
+  const selectTechnologies = document.querySelector('#technologies_id');
+  const selectWorkflows = document.querySelector('#workflows_id');
+  const selectTeams = document.querySelector('#teams_id');
+  const selectAgencies = document.querySelector('#agencies_id');
+  const selectFreelancers = document.querySelector('#freelancers_id');
+  const selectAffiliates = document.querySelector('#affiliates_id');
 
-fetch('https://x8ki-letl-twmt.n7.xano.io/api:Nwul72Uo:v1/projects')
-  .then(response => response.json())
-  .then(data => {
+  fetch('https://x8ki-letl-twmt.n7.xano.io/api:Nwul72Uo:v1/projectForm')
+    .then(response => response.json())
+    .then(data => {
+      
+        hideProgressBar();
+        showForm();
+
+        data.agencies.forEach(agency => {
+          const option = document.createElement('option');
+          option.value = agency.id;
+          option.textContent = agency.agencyName;
+          selectAgencies.appendChild(option);
+        });
+
+        data.freelancers.forEach(freelancer => {
+          const option = document.createElement('option');
+          option.value = freelancer.id;
+          option.textContent = freelancer.freelancerFirstname + ' ' + freelancer.freelancerLastname;
+          selectFreelancers.appendChild(option);
+        });
+
+        data.customers.forEach(customer => {
+          const option = document.createElement('option');
+          option.value = customer.id;
+          option.textContent = customer.customerFirstname + ' ' + customer.customerLastname;
+          selectCustomers.appendChild(option);
+        });
+
+        data.managers.forEach(manager => {
+          const option = document.createElement('option');
+          option.value = manager.id;
+          option.textContent = manager.managerFirstname + ' ' + manager.managerLastname;
+          selectManagers.appendChild(option);
+        });
+
+        data.services.forEach(service => {
+          const option = document.createElement('option');
+          option.value = service.id;
+          option.textContent = service.serviceName;
+          selectServices.appendChild(option);
+        });
+
+        data.technologies.forEach(technology => {
+          const option = document.createElement('option');
+          option.value = technology.id;
+          option.textContent = technology.technologyName;
+          selectTechnologies.appendChild(option);
+        });
+
+        data.workflows.forEach(workflow => {
+          const option = document.createElement('option');
+          option.value = workflow.id;
+          option.textContent = workflow.workflowName;
+          selectWorkflows.appendChild(option);
+        });
+
+        data.teams.forEach(team => {
+          const option = document.createElement('option');
+          option.value = team.id;
+          option.textContent = team.teamFirstname+ ' ' + team.teamLastname;
+          selectTeams.appendChild(option);
+        });
+
+        data.affiliates.forEach(affiliate => {
+          const option = document.createElement('option');
+          option.value = affiliate.id;
+          option.textContent = affiliate.affiliateFirstname + ' ' + affiliate.affiliateLastname;
+          selectAffiliates.appendChild(option);
+        });
+
+    })
+    .catch(error => console.error('Error fetching data:', error));
+
+    function getCurrentDate() {
+      const currentDate = new Date();
     
-      data.agencies.forEach(agency => {
-        const option = document.createElement('option');
-        option.value = agency.id;
-        option.textContent = agency.agencyName;
-        selectAgencies.appendChild(option);
-      });
-
-      data.freelancers.forEach(freelancer => {
-        const option = document.createElement('option');
-        option.value = freelancer.id;
-        option.textContent = freelancer.freelancerLastname;
-        selectFreelancers.appendChild(option);
-      });
-
-      data.customers.forEach(customer => {
-        const option = document.createElement('option');
-        option.value = customer.id;
-        option.textContent = customer.customerLastname;
-        selectCustomers.appendChild(option);
-      });
-
-      data.managers.forEach(manager => {
-        const option = document.createElement('option');
-        option.value = manager.id;
-        option.textContent = manager.managerLastname;
-        selectManagers.appendChild(option);
-      });
-
-      data.services.forEach(service => {
-        const option = document.createElement('option');
-        option.value = service.id;
-        option.textContent = service.serviceName;
-        selectServices.appendChild(option);
-      });
-
-      data.technologies.forEach(technology => {
-        const option = document.createElement('option');
-        option.value = technology.id;
-        option.textContent = technology.technologyName;
-        selectTechnologies.appendChild(option);
-      });
-
-      data.workflows.forEach(workflow => {
-        const option = document.createElement('option');
-        option.value = workflow.id;
-        option.textContent = workflow.workflowName;
-        selectWorkflows.appendChild(option);
-      });
-
-      data.teams.forEach(team => {
-        const option = document.createElement('option');
-        option.value = team.id;
-        option.textContent = team.teamFirstname;
-        selectTeams.appendChild(option);
-      });
-
-      data.affiliates.forEach(affiliate => {
-        const option = document.createElement('option');
-        option.value = affiliate.id;
-        option.textContent = affiliate.affiliateFirstname;
-        selectAffiliates.appendChild(option);
-      });
-
-  })
-  .catch(error => console.error('Error fetching data:', error));
-
-  function getCurrentDate() {
-    const currentDate = new Date();
-  
-    const year = currentDate.getFullYear();
-    const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
-    const day = currentDate.getDate().toString().padStart(2, '0');
-  
-    return `${year}-${month}-${day}`;
-  }
-  
-  document.getElementById("projectStart").value = getCurrentDate();
+      const year = currentDate.getFullYear();
+      const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+      const day = currentDate.getDate().toString().padStart(2, '0');
+    
+      return `${year}-${month}-${day}`;
+    } 
+    document.getElementById("projectStart").value = getCurrentDate();
+}
